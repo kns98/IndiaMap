b4c7be2f3b8db78b2caa6d1568ddfef0eb9dea31
kns98 <kns98@yahoo.com>
2024-01-24 10:18:14 +0800

    Add project files.

---
+++ India/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        string[] resourceNames = currentAssembly.GetManifestResourceNames();
+
+        foreach (string resourceName in resourceNames)
+        {
+            Console.WriteLine(resourceName);
+
+             using (Stream stream = currentAssembly.GetManifestResourceStream(resourceName))
+             {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
